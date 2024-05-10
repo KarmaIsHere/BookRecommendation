7f2806b6f4c771934ac1c5ae17a1dedb45aa4500
KarmaIsHere <letrixswitch@gmail.com>
2024-05-10 21:37:47 +0300

    created simple dialogflow webhook

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=["POST", "GET"])
+def webhook():
+    if request.method == "GET":
+        return "Hello, World!"
+    elif request.method == "POST":
+        payload = request.json
+        user_response = (payload['queryResult']['queryText'])
+        bot_response = (payload['queryResult']['fulfillmentText'])
+        if user_response or bot_response != "":
+            print("User: " + user_response)
+            print("Bot: " + bot_response)
+            return "Message recieved."
+        else:
+            print(request.data)
+            return "200"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
